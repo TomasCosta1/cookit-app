12c9662e793955a4e1759dc25f2fcb02c0499e8f
Juamba02 <juanaramberri02@gmail.com>
2025-10-02 15:26:33 -0300

    Terminada la feature para agregar y quitar ingredientes a la lista del usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Ingredients from './pages/Ingredients.jsx'
+import MyIngredients from './pages/MyIngredients.jsx'
+import { Header } from './components/Header.jsx'
 
 function App() {
-
   return (
-    <>
-      <h1>Cookit!</h1>
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<h1>Cookit!</h1>} />
+        <Route path="/ingredients" element={<Ingredients />} />
+        <Route path="/my-ingredients" element={<MyIngredients />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
