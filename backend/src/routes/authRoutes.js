4e7db2a56c700b174de7b3219f7d04786468f28f
Tomas Costa <totocosta3@gmail.com>
2025-10-04 20:28:12 -0300

    vercel dependencies (#6)

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-const { login, register } = require('../controllers/authcontroller');
+const { login, register } = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/register', register);
