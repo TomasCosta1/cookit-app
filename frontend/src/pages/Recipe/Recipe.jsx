32d0c6bd6e0d4d07b098b98fd8a635cca5bf96b9
Tomas Costa <totocosta3@gmail.com>
2025-10-04 21:07:59 -0300

    Feature/auto deployment (#7)
    * vercel dependencies
    
    * VITE_API_URL for Vercel

--- frontend/src/pages/Recipe/Recipe.jsx
+++ frontend/src/pages/Recipe/Recipe.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import "./Recipe.css";
 import Button from "../../components/Button/Button";
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export default function Recipe() {
     const { id } = useParams();
--- frontend/src/pages/RecipesList/RecipesList.jsx
+++ frontend/src/pages/RecipesList/RecipesList.jsx
@@ -4,7 +4,7 @@ import RecipeCard from "../../components/RecipeCard/RecipeCard";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import Button from "../../components/Button/Button";
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export default function RecipesList() {
     const [q, setQ] = useState("");
